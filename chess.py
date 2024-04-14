dad093c937d3009eae950b981711cb2ad57690de
Brandon Tsai <01.tsaibrandon@gmail.com>
2024-04-13 17:16:25 -0700

    changed column id to letters from numbers

--- chess.py
+++ chess.py
@@ -1,8 +1,11 @@
 def main():
     chess_board()
+    select_piece()
 
 # create chess board
 def chess_board(): 
+    column_letters = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    
     board = [
         ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
         ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
@@ -19,6 +22,13 @@ def chess_board():
             # prints the piece and then adds a space at the end of it instead of going to the next line
             print(piece, end=" ")
         print()
+
+# moving the pieces
+def select_piece():
+    select = input("Which piece would you like to move? ")
+    print(select)
+
+
 
 if __name__ == "__main__":
     main()
