d45a8054bd7fb6503d40702b66984e9a76c4708b
Brandon Tsai <01.tsaibrandon@gmail.com>
2024-04-30 19:11:53 -0700

    redid the board and added an engine

--- chess.py
+++ chess.py
@@ -1,34 +1,109 @@
-def main():
-    chess_board()
-    select_piece()
+player_1 = 1
+player_2 = 2
+whos_turn = 1 
 
-# create chess board
-def chess_board(): 
-    column_letters = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+class Chess:
+    """
+        This engine of the game. Where all the basic rules are kept to make sure the game runs properly.
+    """
+    def __init__(self):
+        self.board = Board()
+
+    def game_engine(self):
+        # if it is not checkmate then the game continues
+        # if checkmate detected then game ends
+
+        return True
+
     
-    board = [
-        ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-        ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-        ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-        ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-        ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-        ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-        ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-        ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+    def start_game(self):
+        # initializes players, board, and pieces
+
+        self.board.create_board()
+
+        while self.game_engine():
+            self.player_turn()
+        
+
+    def player_turn(self):
+        global whos_turn
+        self.check_turn()
+        if self.check_turn() == player_1:
+            print("It's WHITE's turn!")
+            white_select_piece = input("Which piece would you like to move? ")
+            print(white_select_piece)
+            whos_turn += 1 
+            print(whos_turn)
+        else:
+            print("It's BLACK's turn!")
+            black_select_piece = input("Which piece would you like to move? ")
+            print(black_select_piece)
+            whos_turn -= 1
+            print(whos_turn)
+
+    def check_turn(self):
+        if whos_turn == 1:
+            return player_1
+        else:
+            return player_2
+
+    def end_game(self):
+        pass
+
+
+class Board:
+    def __init__(self):
+        self.board = [
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"],
+            ["-", "-", "-", "-", "-", "-", "-", "-"]
         ]
     
-    for row in board:
-        for piece in row:
-            # prints the piece and then adds a space at the end of it instead of going to the next line
-            print(piece, end=" ")
-        print()
-
-# moving the pieces
-def select_piece():
-    select = input("Which piece would you like to move? ")
-    print(select)
+    def create_board(self):
+        for row in self.board:
+            print(" ".join(row))
+    
+    def update_board(self):
+        pass
 
 
+class Piece():
+    def __init__(self, color, id):
+        self.color = color
+        self.id = id
+
+    def check_color(self):
+        pass 
+    
+    def check_type(self):
+        pass
+
+class Pawn(Piece):
+#    ♟     
+    pass                 
+
+class Rook(Piece):
+    pass
+
+class Knight(Piece):
+    pass
+
+class Bishop(Piece):
+    pass
+
+class Queen(Piece):
+    pass
+
+class King(Piece):
+    pass
+    
 
 if __name__ == "__main__":
-    main()
+    game = Chess()
+
+    game.start_game()
