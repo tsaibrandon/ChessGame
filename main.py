f34a8bbf17a8ca55319acf876ed97fc34395d1e0
Brandon Tsai <01.tsaibrandon@gmail.com>
2025-03-20 02:09:26 -0700

    trying to add piece for player validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from game import Chess
+from board import Board
 
 def valid_input(input):
     if len(input) == 2:
@@ -13,6 +14,7 @@ def valid_input(input):
 
 def main():
     game = Chess()
+    board = Board()
     game.start_game()
     
     # Game loop
@@ -20,9 +22,19 @@ def main():
         print(f"It's {game.turn}'s turn")
 
         while True:
-            first_input = input("Select a piece you want to move (e.g. 'e2'): ")
+            first_input = input("Enter the square of your piece (e.g. 'e2'): ")
             if not valid_input(first_input):
                 continue
+            else:
+                piece_color = board.get_piece_color(board.get_piece_at(first_input))
+                print(piece_color)
+
+                if piece_color == None:
+                    print("There is no piece there! Please select one of your pieces.")
+                    continue
+                elif piece_color != game.turn:
+                    print("This is not your piece. Please select one of your pieces.")
+                    continue
 
             second_input = input("Select a square to move to (e.g. 'e4'): ")
             if not valid_input(second_input):
